185da7ecd061b5e98dd92ffc8168a1ad84df76d8
Ekaterina <ed2408@yandex.ru>
2021-12-14 22:45:11 +0300

    Minimum

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,21 @@
+package ru.skypro;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getAuthorFirstName() {
+        return firstName;
+    }
+
+    public String getAuthorLastName() {
+        return lastName;
+    }
+
+
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,15 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//       Инициализация авторов
+        Author king = new Author("Steven", "King");
+        Author nabokov = new Author("Vladimir", "Nabokov");
+//        Инициализация книг
+        Book misery = new Book("Misery", king, 1990);
+        Book lolita = new Book("Lolita", nabokov, 1955);
+
+//         Изменение года
+        misery.setYear(2015);
+
     }
 }
